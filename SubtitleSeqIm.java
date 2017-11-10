2c23c03864caae04d78dfcb66504fb713ed4d36b
Os <OsamaAlfaify@users.noreply.github.com>
2017-11-10 23:56:44 +0300

    osama Alfaify

--- SubtitleSeqIm.java
+++ SubtitleSeqIm.java
@@ -2,33 +2,52 @@
 public class SubtitleSeqIm implements SubtitleSeq {
 	public LinkedList<Subtitle> sub;
 
-
 	public SubtitleSeqIm() {
 		sub = new LinkedList<Subtitle>();
 	}
 
-
-
-
 	public void addSubtitle(Subtitle st) {
-		
 
 		if (sub.full())
 			return;
-		if(sub.empty()){
+		if(st ==null)
+			return;
+		
+		if (sub.empty()) {
+			System.out.println("1");
 			sub.insert(st);
 			return;
 		}
-		Time1 given = (Time1) st.getStartTime();
 		sub.findFirst();
-		while(((Time1) sub.retrieve().getEndTime()).timeToMS() < given.timeToMS() && !sub.last()){
+		int at = ((TimeIm) st.getStartTime()).timeToMS();
+		while (!sub.last()) {
+			int ac = ((TimeIm) sub.retrieve().getEndTime()).timeToMS();
+			if (ac <= at) {
+				sub.insert(st);
+				System.out.println("2");
+				return;
+			}
 			sub.findNext();
 		}
-		if(((Time1) sub.retrieve().getEndTime()).timeToMS() < given.timeToMS()&& !!sub.empty())
-			sub.findNext();
-		sub.insert(st);
+		int ac = ((TimeIm) sub.retrieve().getEndTime()).timeToMS();
+		if (ac <= at) {
+			sub.insert(st);
+			System.out.println("3");
+			return;
 		}
-	
+		System.out.println("4");
+
+			//
+			//// while(((Time1) sub.retrieve().getEndTime()).timeToMS() < &&
+			// !sub.last()){
+			//// sub.findNext();
+			//// }
+			//// if(((Time1) sub.retrieve().getEndTime()).timeToMS() <
+			// given.timeToMS()&& !!sub.empty())
+			//// sub.findNext();
+			//// sub.insert(st);
+		
+	}
 
 	@Override
 	public List<Subtitle> getSubtitles() {
@@ -49,23 +68,24 @@ public class SubtitleSeqIm implements SubtitleSeq {
 		sub.findFirst(); // need to start from the fist subtitle
 
 		while (!sub.last()) {
-			if (inTime((Time1)time)) // returns true if the given time match the time
-								// in the current
+			if (inTime((TimeIm) time)) // returns true if the given time match
+										// the time
+				// in the current
 				return sub.retrieve();
 			sub.findNext();
 		}
-		if (inTime((Time1)time)) // checking the last element
+		if (inTime((TimeIm) time)) // checking the last element
 			return sub.retrieve();
 		return null;
 	}
 
-	private boolean inTime(Time1 time) { // created to make code cleaner and
-										// might need to use the some method
-										// again
+	private boolean inTime(TimeIm time) { // created to make code cleaner and
+											// might need to use the some method
+											// again
 
-		Time1 start =(Time1) sub.retrieve().getStartTime();
-		Time1 end = (Time1)sub.retrieve().getEndTime();
-		double startt = start.timeToMS(); 
+		TimeIm start = (TimeIm) sub.retrieve().getStartTime();
+		TimeIm end = (TimeIm) sub.retrieve().getEndTime();
+		double startt = start.timeToMS();
 		double endt = end.timeToMS();
 		double givent = time.timeToMS();
 		if (givent >= startt && givent < endt)
@@ -142,40 +162,48 @@ public class SubtitleSeqIm implements SubtitleSeq {
 
 	@Override
 	public void shift(int offset) {
-	if(sub.empty())
-		return;
-	sub.findFirst();
-		while(!sub.last()){
+		if (sub.empty())
+			return;
+		sub.findFirst();
+		while (!sub.last()) {
 			Subtitle temp = sub.retrieve();
-			((Time1) temp.getStartTime()).timeShifter(offset); //shifting the start time by the given offset
-			((Time1) temp.getEndTime()).timeShifter(offset); //shifting the end time by the given offset
-			sub.update(temp); 
+			((TimeIm) temp.getStartTime()).timeShifter(offset); // shifting the
+																// start time by
+																// the given
+																// offset
+			((TimeIm) temp.getEndTime()).timeShifter(offset); // shifting the
+																// end time by
+																// the given
+																// offset
+			sub.update(temp);
 			sub.findNext();
 		}
-		if(sub.last()){ //checking the last element
+		if (sub.last()) { // checking the last element
 			Subtitle temp = sub.retrieve();
-			((Time1) temp.getStartTime()).timeShifter(offset); //shifting the start time by the given offset
-			((Time1) temp.getEndTime()).timeShifter(offset); //shifting the end time by the given offset
-			sub.update(temp); 
+			((TimeIm) temp.getStartTime()).timeShifter(offset); // shifting the
+																// start time by
+																// the given
+																// offset
+			((TimeIm) temp.getEndTime()).timeShifter(offset); // shifting the
+																// end time by
+																// the given
+																// offset
+			sub.update(temp);
 		}
-	
 
 	}
 
 	@Override
 	public void cut(Time startTime, Time endTime) {
 
-		
-
-
 	}
-	
+
 	// testing only
-	public void print(){
-		if(sub.empty())
+	public void print() {
+		if (sub.empty())
 			return;
 		sub.findFirst();
-		while(!sub.last()){
+		while (!sub.last()) {
 			SubtitleIm a = (SubtitleIm) sub.retrieve();
 			System.out.println(a.toString());
 			sub.findNext();
