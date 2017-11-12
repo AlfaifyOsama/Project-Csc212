bc4935cfc5ab91542a702d9949f99fb2ad31c6eb
Os <OsamaAlfaify@users.noreply.github.com>
2017-11-12 19:05:25 +0300

    Cut Method

--- SubtitleSeqFactory.java
+++ SubtitleSeqFactory.java
@@ -3,8 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class SubtitleSeqFactory {
- 
-	// Return an empty subtitles sequence 
+
+	// Return an empty subtitles sequence
 	public static SubtitleSeq getSubtitleSeq() {
 		return new SubtitleSeqIm();
 	}
@@ -17,44 +17,48 @@ public class SubtitleSeqFactory {
 		String pattern = "^\\d{2}+[:]+\\d{2}+[:]+\\d{2}+[,]+\\d{3}+ --> +\\d{2}+[:]+\\d{2}+[:]+\\d{2}+[,]+\\d{3}$";
 		try {
 			Scanner in = new Scanner(new File(fileName));
-			while(in.hasNext()){
-				
-				//Check if it has valid sequential number
+			while (in.hasNext()) {
+
+				// Check if it has valid sequential number
 				String seqNumString = in.nextLine();
-				if(!seqNumString.equals("" + seqNum++)){
+				if (!seqNumString.equals("" + seqNum++)) {
 					return null;
 				}
-				
-				//Check if it has valid time pattern
+
+				// Check if it has valid time pattern
 				String time = in.nextLine();
-				if(!time.matches(pattern)){
+				if (!time.matches(pattern)) {
 					return null;
 				}
 				String[] timeArray = time.split(" ");
 				String startTime = timeArray[0];
 				String endTime = timeArray[2];
-				
-				//Skip text line
+
+				// Skip text line
 				String text = in.nextLine();
-				
-				//Check if it is not the last subtitle, then check for break line
-				if(in.hasNext()){
-					if(!in.nextLine().equals("")){
+
+				// Check if it is not the last subtitle, then check for break
+				// line
+				if (in.hasNext()) {
+					if (!in.nextLine().equals("")) {
 						return null;
 					}
 				}
-				
-				
-				//add to the subtitleSeq
-				Time startTimeObj = new TimeIm(Integer.parseInt(startTime.substring(0, 2)), Integer.parseInt(startTime.substring(3,5)), Integer.parseInt(startTime.substring(6,8)), Integer.parseInt(startTime.substring(9,12)));
-				Time endTimeObj = new TimeIm(Integer.parseInt(endTime.substring(0, 2)), Integer.parseInt(endTime.substring(3,5)), Integer.parseInt(endTime.substring(6,8)), Integer.parseInt(endTime.substring(9,12)));
+
+				// add to the subtitleSeq
+				Time startTimeObj = new TimeIm(Integer.parseInt(startTime.substring(0, 2)),
+						Integer.parseInt(startTime.substring(3, 5)), Integer.parseInt(startTime.substring(6, 8)),
+						Integer.parseInt(startTime.substring(9, 12)));
+				Time endTimeObj = new TimeIm(Integer.parseInt(endTime.substring(0, 2)),
+						Integer.parseInt(endTime.substring(3, 5)), Integer.parseInt(endTime.substring(6, 8)),
+						Integer.parseInt(endTime.substring(9, 12)));
 				SubtitleIm tmp = new SubtitleIm(startTimeObj, endTimeObj, text);
 				seq.addSubtitle(tmp);
-				
+
 			}
-		} catch (FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			return null;
-		} catch (Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 		return seq;
